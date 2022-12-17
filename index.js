04eb99258050d1f2b7990ed2243068792b0d19b0
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2022-12-17 19:06:39 +0900

    feat: create seviceWorker.js
    1. vanilla js로 serviceWorker.js 생성 및 등록

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-window.addEventListener('load', e => {
+window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
+
+    if ('serviceWorker' in navigator){
+        try {
+            navigator.serviceWorker.register('serviceWorker.js');
+            console.log("Service Worker Registered");
+        } catch (error) {
+            console.log("Service Worker Registration Failed");
+        }
+    }
 
     pads.forEach((pad, index) => {
         pad.addEventListener('click', e => {
